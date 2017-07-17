dda620c463b905ce79214bb42d72f70b8e0f13b4
dba <dbgsm0@c4b301b857f3.hq.target.com>
2017-07-17 10:15:20 +0530

    first commit

--- git-push.sh
+++ git-push.sh
@@ -1,4 +1,4 @@
 git add .
 git commit -m "first commit"
-git remote add origin git@github.com:coderdba/sensu-grafana.git
+#git remote add origin git@github.com:coderdba/sensu-grafana.git
 git push -u origin master
